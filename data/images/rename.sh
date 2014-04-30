769bc02c142bcb453e789e50354082fd8c559599
Jan-Willem van de Meent <jwvdm@robots.ox.ac.uk>
2014-04-30 18:04:25 -0400

    added new images, deleted old composite

--- data/images/rename.sh
+++ data/images/rename.sh
@@ -1,7 +1,10 @@
-cat ../levantine_corpus_gansell.csv | while read l
+cat ../levantine_corpus_gansell.csv | awk '{printf("%d,%s\n", NR-1, $0)}' | while read l
 do 
     id=`echo $l | cut -d, -f1`
     name=`echo $l | cut -d, -f2`
     mv "$id.jpg" "$name.jpg"
-    touch "$name.jpg"
+    if [ -e "$name.jpg" ]
+    then
+        touch "$name.jpg"
+    fi
 done
